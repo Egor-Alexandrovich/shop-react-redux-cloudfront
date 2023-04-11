958a667eedc6a210ab3711a0d8308a67fa0cb206
Yahor Shulha <yahor_shulha@epam.com>
2023-04-11 19:42:23 +0300

    Add api import path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://0dey54hzz5.execute-api.us-east-1.amazonaws.com",
   order: "https://0dey54hzz5.execute-api.us-east-1.amazonaws.com",
-  import: "https://0dey54hzz5.execute-api.us-east-1.amazonaws.com",
+  import: "https://8ku2x40mcc.execute-api.us-east-1.amazonaws.com",
   bff: "https://0dey54hzz5.execute-api.us-east-1.amazonaws.com",
   cart: "https://0dey54hzz5.execute-api.us-east-1.amazonaws.com",
 };
