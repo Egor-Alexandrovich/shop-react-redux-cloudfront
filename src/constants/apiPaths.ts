78abd1eba0e3fe6653d52f59191440c1cbf92c9a
Yahor Shulha <yahor_shulha@epam.com>
2023-04-28 10:55:27 +0300

    Add authorization header

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://0dey54hzz5.execute-api.us-east-1.amazonaws.com",
-  order: "https://0dey54hzz5.execute-api.us-east-1.amazonaws.com",
-  import: "https://8ku2x40mcc.execute-api.us-east-1.amazonaws.com",
-  bff: "https://0dey54hzz5.execute-api.us-east-1.amazonaws.com",
-  cart: "https://0dey54hzz5.execute-api.us-east-1.amazonaws.com",
+  product: "https://5r2mxlx22j.execute-api.us-east-1.amazonaws.com",
+  order: "https://5r2mxlx22j.execute-api.us-east-1.amazonaws.com",
+  import: "https://v0i9wtxrbh.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://5r2mxlx22j.execute-api.us-east-1.amazonaws.com",
+  cart: "https://5r2mxlx22j.execute-api.us-east-1.amazonaws.com",
 };
 
 export default API_PATHS;
