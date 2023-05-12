b33f1dee320c50f356909c2fd38c34030547e4b9
Yahor Shulha <yahor_shulha@epam.com>
2023-05-12 21:49:43 +0300

    Implement task

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://5r2mxlx22j.execute-api.us-east-1.amazonaws.com",
-  order: "https://5r2mxlx22j.execute-api.us-east-1.amazonaws.com",
+  product: "https://5eqj3ewg27.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://5eqj3ewg27.execute-api.us-east-1.amazonaws.com/dev",
   import: "https://v0i9wtxrbh.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://5r2mxlx22j.execute-api.us-east-1.amazonaws.com",
-  cart: "https://5r2mxlx22j.execute-api.us-east-1.amazonaws.com",
+  cart: "https://5eqj3ewg27.execute-api.us-east-1.amazonaws.com/dev/api",
 };
 
 export default API_PATHS;
